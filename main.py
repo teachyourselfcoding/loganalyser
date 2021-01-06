f7805a3922a7d3787722d53ddf990b15c08a261f
teachyourselfcoding <e0310446@u.nus.edu>
2021-01-06 22:32:28 +0800

    try

--- main.py
+++ main.py
@@ -2,28 +2,37 @@ import csv
 import numpy as np
 import pandas as pd
 import json
+import re
 from matplotlib import pyplot as plt
 
-testfile = "/Users/zouyanfeng/PycharmProjects/pythonProject/venv/testfile.txt"
-filename = "/Users/zouyanfeng/PycharmProjects/pythonProject/venv/log_zu5_case2.txt"  # Path to log txt file
-unsub = "ADASCipvEstimationOP::Set (ID | CIPV | LOC | TTC | dis.x | dis.y )"
+testfile = "/Users/zouyanfeng/PycharmProjects/pythonProject/loganalyser/logtest.txt"
+filename = "/Users/zouyanfeng/PycharmProjects/pythonProject/loganalyser/log_zu5_case2.txt"  # Path to log txt file
+
+
+
+#############KEYS
 perceptionkey = "ADASCipvEstimationOP:: Process end![TimeCost]"
+coordinateskey = "ADASCipvEstimationOP::Set (ID | CIPV | LOC | TTC | dis.x | dis.y )"
+framecountkey = "current frame N"
+
 # Unsub contains the keyword of the estimator data
 
 
 
 list = []
-xlist = []
-ylist = []
 
 
-errorline = 0
+objectlibrary = {}
+#Create dictionary of objects identified
+
+
 fps = []
-
+linecount = 0
 
 def main():
 
-    fileimport(filename, unsub, errorline)
+
+    fileimport(testfile)
     # Start txt file import process, will process text and return an array of coordinates back to main function
 
     # convertjson()
@@ -32,25 +41,44 @@ def main():
     # plotpoints()
     # Plot x,y points in pylots
 
+    # for keys, values in objectlibrary.items():
+    #     print(keys)
+    #     # zipped = "\n".join("{} {}".format(x, y) for x, y in zip(values[0], values[1]))
+    #     for t1,t2 in zip(values[0], values[1]):
+    #         print('%-20s %s' % (t1, t2))
+    #Manual testing: Printing of 3D array
 
-def fileimport(filepath, unsub, errorline):
+
+def fileimport(filepath):
+    global linecount
     with open(filepath, 'r') as f:
         lines = csv.reader(f)
         for line in lines:
-            errorline = errorline + 1
-
-            if unsub in str(line):
-                data = str(line).split(unsub)[1]
-                textprocess(data, errorline, line)
-                # textprocess function will extract x and y information based on the current log text syntax
-            else if 
-
+            linecount = linecount + 1
+            (textprocess(str(line)))
     f.close()
 
 
-def textprocess(datastring, errorline, line):
+def textprocess(line):
+    if coordinateskey in line:
+        extractcoordinates(line)
+        # textprocess function will extract x and y information based on the current log text syntax
+
+    if perceptionkey in line:
+        extractfps(line)
+
+    if framecountkey in line:
+        extractframecount(line)
+
+
+
+
+def extractcoordinates(line):
+
+    data = str(line).split(coordinateskey)[1]
     try:
-        datalist = datastring.split("|")
+        datalist = data.split("|")
+        objectid = int(datalist[0].strip())
         xvalue = float(datalist[4].strip())
         yvalue = float(datalist[5].strip()[:-2:])
 
@@ -59,22 +87,58 @@ def textprocess(datastring, errorline, line):
         print("[error] fpga_can_client.cpp99 [FpgaCanClient] receive message failed")
         return
 
-    xlist.append(xvalue)
-    ylist.append(yvalue)
+    # print(objectid, xvalue, yvalue)
 
-    newarray = [xvalue, yvalue]
-    #     # add x,y coordinate to the 3D array
-    addarray(newarray)
+    if objectid != 20001:
+        print(objectid)
+    #Manual test: To check if there is other object ID-ed
+
+    if objectlibrary.get(objectid) == None:
+
+        #New object identified
+
+        xlist = []
+        ylist = []
+        objectlibrary[objectid] = [xlist, ylist]
+        #New array added to array
 
 
-def addarray(array):
-    list.append(array)
+    objectlibrary[objectid][0].append(xvalue)
+    objectlibrary[objectid][1].append(yvalue)
+    combinedarray = [xvalue, yvalue]
+    #add x,y coordinate to the 3D array
+
+
+def extractfps(line):
+    try:
+        data = str(line).split("32mPerception")[1]
+        start = "32m"
+        end = "ms"
+        want = data[data.find(start) + len(start):data.rfind(end)]
+        fps = round(1 / float(want)*1000)
+    except:
+        print(line)
+    # print(str(fps) + " fps")
+
+
+def extractframecount(line):
+    frameid = str(line).split(framecountkey)[1].strip()
+    # frameid = int(frameid[1:-2])
+
+    # print("frameid" + str(frameid))
+
+
+
+
+
+
 
 
 def plotpoints():
     # plt.scatter(xlist, ylist)
     plt.plot(xlist, ylist)
     plt.show()
+
 
 
 def convertjson():
